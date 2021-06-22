c9680a14d2b4c89c4b88311ea9925da35504961f
Anton Chepelev <chepelash@gmail.com>
2021-06-23 00:27:52 +0300

    Added helper print

--- helpers/helper.h
+++ helpers/helper.h
@@ -1,5 +1,11 @@
-#define PERR_EXIT(msg)   \
+#define PERR_EXIT(msg)  \
 ({                      \
     perror(msg);        \
     exit(EXIT_FAILURE); \
-})
+}) 
+
+#define UERR_EXIT(msg, pname)   \
+({                              \
+    printf(msg, pname);         \
+    exit(EXIT_FAILURE);         \
+}) 
